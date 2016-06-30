28842c44f5e6253ffa31ed332ce606328f40f859
immortalCockroach <buptzhangyi@gmail.com>
2016-06-30 22:17:45 +0800

    设置topology的基本参数

--- src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
@@ -1,10 +1,36 @@
 package com.alibaba.middleware.race.jstorm;
 
+import com.alibaba.middleware.race.RaceConfig;
+
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+
 public class RaceTopology {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Config conf = new Config();
+		
+		conf.setNumAckers(1);
+		conf.setNumWorkers(3);
+        int spout_Parallelism_hint = 4;
+        int split_Parallelism_hint = 4;
+        int count_Parallelism_hint = 4;
+        conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 100000);
+        
+        TopologyBuilder builder = new TopologyBuilder();
+        
+        builder.setSpout("topicSpout", new TopicEmitSpout(),spout_Parallelism_hint);
+        
+        String topologyName = RaceConfig.JstormTopologyName;
 
+        try {
+            StormSubmitter.submitTopology(topologyName, conf, builder.createTopology());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 	}
 
 }
