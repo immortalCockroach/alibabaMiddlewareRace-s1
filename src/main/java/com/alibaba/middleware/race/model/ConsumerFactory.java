0bb3638fd8137618b61a0c65a9ccd28f86ad2afd
immortalCockroach <buptzhangyi@gmail.com>
2016-06-30 20:29:15 +0800

    构造comsumer的factory，其中设置consumer的订阅的topic信息，消费线程数，拉取的消息数和批量的消费数

---
+++ src/main/java/com/alibaba/middleware/race/model/ConsumerFactory.java
@@ -0,0 +1,32 @@
+package com.alibaba.middleware.race.model;
+
+import com.alibaba.middleware.race.RaceConfig;
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+
+public class ConsumerFactory {
+	public static synchronized DefaultMQPushConsumer mkPushConsumerInstance(MessageListenerConcurrently listener) throws MQClientException {
+		DefaultMQPushConsumer pushConsumer = new DefaultMQPushConsumer(RaceConfig.RocketMQGroup);
+		
+		pushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+		// 正式提交的时候去掉这行
+		pushConsumer.setNamesrvAddr("10.109.247.167:9876");
+		// consumer.start();
+		pushConsumer.subscribe(RaceConfig.MqPayTopic, "*");
+		pushConsumer.subscribe(RaceConfig.MqTaobaoTradeTopic, "*");
+		pushConsumer.subscribe(RaceConfig.MqTmallTradeTopic, "*");
+
+		pushConsumer.setConsumeMessageBatchMaxSize(16);
+		pushConsumer.setPullBatchSize(64);
+		
+		pushConsumer.registerMessageListener(listener);
+		pushConsumer.setConsumeThreadMin(2);
+		pushConsumer.setConsumeThreadMin(4);
+		pushConsumer.start();
+		
+		return pushConsumer;
+	}
+}
