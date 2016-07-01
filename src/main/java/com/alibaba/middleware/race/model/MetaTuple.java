1ae1f86d19f9f48c47b21d5f7f6e5b659a773ce7
immortalCockroach <buptzhangyi@gmail.com>
2016-07-01 23:00:44 +0800

    将MetaTuple作为PayMessage的封装类型，其中包含了fail的次数和每次失败后发送的时延，超过5次失败后丢弃

--- src/main/java/com/alibaba/middleware/race/model/MetaTuple.java
+++ src/main/java/com/alibaba/middleware/race/model/MetaTuple.java
@@ -1,32 +1,45 @@
 package com.alibaba.middleware.race.model;
 
+import edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch;
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
+
 /**
- * 为了将3个不同的消息统一封装进入阻塞队列而定义的封装结构
+ * 为了PaymentMessage的流量和重发控制而定义的封装结构,设置了最大重发次数5，超过5次丢弃
+ * 
  * @author 打不死的小强
  *
  */
 public class MetaTuple {
-	private String topicIdentifier;
-	private Object message;
+	private AtomicInteger failTimes;
 
-	public MetaTuple(String topicIdentifier, Object message) {
-		this.topicIdentifier = topicIdentifier;
+	private PaymentMessage message;
+	public static final int MAX_FAIL_TIMES = 5;
+	private CountDownLatch latch;
+
+	public MetaTuple(PaymentMessage message) {
 		this.message = message;
+		this.failTimes = new AtomicInteger(0);
+		latch = new CountDownLatch(1);
 	}
 
-	public String getTopicIdentifier() {
-		return topicIdentifier;
-	}
-
-	public void setTopic(String topicIdentifier) {
-		this.topicIdentifier = topicIdentifier;
-	}
-
-	public Object getMessage() {
+	public PaymentMessage getMessage() {
 		return message;
 	}
 
-	public void setMessage(Object message) {
+	public void waitForEmit() throws InterruptedException {
+		latch.await(1, TimeUnit.SECONDS);
+	}
+
+	public int getFailTimes() {
+		return failTimes.intValue();
+	}
+
+	public void incrFailTimes() {
+		this.failTimes.incrementAndGet();
+	}
+
+	public void setMessage(PaymentMessage message) {
 		this.message = message;
 	}
 
