a2febd5d2d6368a42d3481bfa774159c195fa7c6
immortalCockroach <buptzhangyi@gmail.com>
2016-07-01 23:21:10 +0800

    flowControl修改为针对pay消息，其他的消息直接发送，此外设置了pay消息fail时的重发和flowControl与非flowControl模式的切换

--- src/main/java/com/alibaba/middleware/race/jstorm/TopicEmitSpout.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/TopicEmitSpout.java
@@ -2,7 +2,7 @@ package com.alibaba.middleware.race.jstorm;
 
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,14 +28,22 @@ import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
 
 public class TopicEmitSpout implements IRichSpout, MessageListenerConcurrently, IAckValueSpout, IFailValueSpout {
 
 	private static final Logger logger = LoggerFactory.getLogger(TopicEmitSpout.class);
 	private SpoutOutputCollector spoutCollector;
 	private DefaultMQPushConsumer consumer;
-	private LinkedBlockingDeque<MetaTuple> emitQueue;
-	private boolean flowControl;
+	private LinkedBlockingQueue<MetaTuple> emitQueue;
+	// flowControl只针对pay消息,防止pay消息过多而在ProcessBolt中产生堆积（而Order消息必须堆积，所以暂时不考虑流量控制）
+	private volatile boolean flowControl;
+
+	// 在flowControl模式下，ackNumber累计到一定程度就取消flowControl
+	private AtomicInteger ackNumber;
+
+	// 在非flowControl模式下 failNumber累积到一定程度就重新设置flowControl
+	private AtomicInteger failNumber;
 
 	@Override
 	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
@@ -45,10 +53,17 @@ public class TopicEmitSpout implements IRichSpout, MessageListenerConcurrently, 
 			consumer = ConsumerFactory.mkPushConsumerInstance(this);
 		} catch (MQClientException e) {
 			// TODO Auto-generated catch block
-			logger.error("failed to create pushconsumer", e);
+			logger.error("ZY spout failed to create pushconsumer", e);
 		}
-		emitQueue = new LinkedBlockingDeque<>();
-		flowControl = false;
+		emitQueue = new LinkedBlockingQueue<MetaTuple>();
+		// 初始设置flowControl为true 防止一开始pay消息发送过多，在ProcessBolt中找不到对应的Order而产生fail信息
+		flowControl = true;
+
+		ackNumber = new AtomicInteger(0);
+		failNumber = new AtomicInteger(0);
+
+		logger.info("ZY spout init finished.");
+
 	}
 
 	@Override
@@ -61,37 +76,40 @@ public class TopicEmitSpout implements IRichSpout, MessageListenerConcurrently, 
 			body = msg.getBody();
 
 			if (body.length == 2 && body[0] == 0 && body[1] == 0) {
-				logger.info(topic + "ends soon");
+				logger.info("ZY spout" + topic + "ends soon");
 				continue;
 			}
 
 			switch (topic) {
 			case RaceConfig.MqPayTopic:
+				// 用字符串0代表pay消息
 				PaymentMessage paymentMessage = RaceUtils.readKryoObject(PaymentMessage.class, body);
+				// 在flowControl模式下 使用阻塞队列来发送Pay的信息
 				if (flowControl) {
-					emitQueue.offer(new MetaTuple("0", paymentMessage));
+					try {
+						emitQueue.put(new MetaTuple(paymentMessage));
+					} catch (InterruptedException e) {
+						logger.info("ZY spout put PAY operation interupt:" + e.getMessage(), e);
+					}
 				} else {
-					sendTuple("0", paymentMessage);
+					sendPayMessage(new MetaTuple(paymentMessage));
 				}
 				break;
+
 			case RaceConfig.MqTmallTradeTopic:
+				// 1代表Tmall消息
 				OrderMessage tmallMessage = RaceUtils.readKryoObject(OrderMessage.class, body);
-				if (flowControl) {
-					emitQueue.offer(new MetaTuple("1", tmallMessage));
-				} else {
-					sendTuple("1", tmallMessage);
-				}
+				sendOrderMessage(RaceConfig.TmallIdentifier, tmallMessage);
 				break;
+
 			case RaceConfig.MqTaobaoTradeTopic:
+				// 2带代表Taobao消息
 				OrderMessage taobaoMessage = RaceUtils.readKryoObject(OrderMessage.class, body);
-				if (flowControl) {
-					emitQueue.offer(new MetaTuple("2", taobaoMessage));
-				} else {
-					sendTuple("2", taobaoMessage);
-				}
+				sendOrderMessage(RaceConfig.TaobaoIdentifier, taobaoMessage);
 				break;
+
 			default:
-				logger.error("contains topic except of 3 above" + topic);
+				logger.error("ZY spout contains topic except of 3 above" + topic);
 				break;
 			}
 		}
@@ -99,24 +117,26 @@ public class TopicEmitSpout implements IRichSpout, MessageListenerConcurrently, 
 	}
 
 	/**
-	 * 阻塞队列emit
+	 * Pay的发送方式，可能是直接发送或者阻塞队列发送(由于可能重发，因此需要设置failTime等)
+	 * 
 	 * @param tuple
 	 */
-	public void sendMetaTuple(MetaTuple tuple) {
-		List<Object> values = new Values(tuple.getTopicIdentifier(), tuple.getMessage());
-		
-		// 将message作为messageId
-		spoutCollector.emit(values, tuple.getMessage());
+	private void sendPayMessage(MetaTuple payMessage) {
+		List<Object> values = new Values(RaceConfig.PayIdentifier, payMessage);
+
+		// 将MetaTuple作为messageId
+		spoutCollector.emit(values, payMessage);
 	}
 
 	/**
-	 * 直接emits 
+	 * 订单的发送方式
+	 * 
 	 * @param topicIdentifier
 	 * @param message
 	 */
-	public void sendTuple(String topicIdentifier, Object message) {
+	private void sendOrderMessage(String topicIdentifier, OrderMessage message) {
 		List<Object> values = new Values(topicIdentifier, message);
-		// 将message作为messageId
+		// 将Message对象作为messageId
 		spoutCollector.emit(values, message);
 	}
 
@@ -128,29 +148,68 @@ public class TopicEmitSpout implements IRichSpout, MessageListenerConcurrently, 
 
 	@Override
 	public void nextTuple() {
-		MetaTuple metaTuple = null;
+		MetaTuple payMessage = null;
 		try {
-			metaTuple = emitQueue.take();
+			payMessage = emitQueue.take();
 		} catch (InterruptedException e) {
+			logger.info("ZY spout take operation interupt:" + e.getMessage(), e);
 		}
 
-		if (metaTuple == null) {
+		if (payMessage == null) {
 			return;
 		}
 
-		sendMetaTuple(metaTuple);
+		sendPayMessage(payMessage);
 
 	}
 
 	@Override
 	public void fail(Object msgId, List<Object> values) {
-		// TODO Auto-generated method stub
-
+		// 如果非flowControl下 failNum数量过大，且超过20%则进入flowControl模式
+		if (!flowControl) {
+			int failNum = failNumber.incrementAndGet();
+			int ackNum = ackNumber.get();
+			if (failNum >= RaceConfig.ThresholdFail && failNum != 0 && ((ackNum / failNum) <= 5)) {
+				logger.info("ZY spout enables flowControl.");
+				flowControl = true;
+				ackNumber.set(0);
+				failNumber.set(0);
+			}
+		}
+		// 说明是pay的信息
+		if (msgId instanceof MetaTuple) {
+			MetaTuple tuple = (MetaTuple) values.get(1);
+			tuple.incrFailTimes();
+			// 重发次数大于MAX则抛弃
+			if (tuple.getFailTimes() <= MetaTuple.MAX_FAIL_TIMES) {
+				try {
+					tuple.waitForEmit();
+				} catch (InterruptedException e) {
+					logger.info("ZY spout pay wait for re-emit interrupt:" + e.getMessage(), e);
+				}
+				spoutCollector.emit(values, msgId);
+			} else {
+				logger.warn("ZY spout payMessage failed more than 5 times,payMsg:" + tuple.getMessage());
+			}
+		} else {
+			// 订单信息直接重新发送
+			spoutCollector.emit(values, msgId);
+		}
 	}
 
 	@Override
 	public void ack(Object msgId, List<Object> values) {
-		// TODO Auto-generated method stub
+		// 在flowControl下，如果ack数量累积到10w(10s预估的偏移量)并且ack和fail比值大于20，则取消flowControl
+		if (flowControl) {
+			int ackNum = ackNumber.incrementAndGet();
+			int failNum = failNumber.get();
+			if (ackNum >= RaceConfig.ThresholdACK && failNum != 0 && ((ackNum / failNum) >= 20)) {
+				logger.info("ZY spout disables flowControl.");
+				flowControl = false;
+				ackNumber.set(0);
+				failNumber.set(0);
+			}
+		}
 
 	}
 
