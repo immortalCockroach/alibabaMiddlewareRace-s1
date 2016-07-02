888169f7bc8bb17a4b68ae9b8f7e34f8c2b34919
immortalCockroach <buptzhangyi@gmail.com>
2016-07-02 18:31:44 +0800

    增加注释

--- src/main/java/com/alibaba/middleware/race/model/ConsumerFactory.java
+++ src/main/java/com/alibaba/middleware/race/model/ConsumerFactory.java
@@ -10,10 +10,11 @@ public class ConsumerFactory {
 	public static synchronized DefaultMQPushConsumer mkPushConsumerInstance(MessageListenerConcurrently listener) throws MQClientException {
 		DefaultMQPushConsumer pushConsumer = new DefaultMQPushConsumer(RaceConfig.RocketMQGroup);
 		
-		pushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+		// 系统自动设置,提交的时候去掉
+		 //pushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 
 		// 正式提交的时候去掉这行
-//		pushConsumer.setNamesrvAddr("10.109.247.167:9876");
+		//pushConsumer.setNamesrvAddr("10.109.247.167:9876");
 		// consumer.start();
 		pushConsumer.subscribe(RaceConfig.MqPayTopic, "*");
 		pushConsumer.subscribe(RaceConfig.MqTaobaoTradeTopic, "*");
